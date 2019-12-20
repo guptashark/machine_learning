b65f617df7dfef812e22d73d35865d8e6b02d521
Aishwary Gupta <guptashar@protonmail.com>
2019-12-20 04:40:43 -0500

    Declare the Lasso regression fn's
    include/linear_model.h:
     * declare the Lasso regression class
     * declare the fit, predict and score fn's.

--- include/linear_model.h
+++ include/linear_model.h
@@ -18,9 +18,9 @@ struct Predictor {
 
 namespace linear_model {
 
-	struct LinearRegression : public Predictor {
+	using array_like = std::vector<std::vector<double> >;
 
-		using array_like = std::vector<std::vector<double> >;
+	struct LinearRegression : public Predictor {
 
 		bool fit_intercept = true;
 		bool normalize = false;
@@ -41,5 +41,28 @@ namespace linear_model {
 		double score
 		(const array_like &X,
 		 const std::vector<double> &y);
+	};
+
+	struct Lasso : public Predictor {
+
+		// if it's 0.0, then we have simple LinearRegression.
+		double alpha = 1.0;
+
+		// calculate intercept, or set to be 0.
+		// (true means calc)
+		bool fit_intercept = true;
+
+		// these should not be set, they're what we're computing.
+		std::vector<double> coef_ ;
+		double intercept_ ;
+
+		// the methods themselves.
+		Lasso & fit
+		(const array_like &X, const std::vector<double> &y);
+
+		std::vector<double> predict(const array_like & X);
+
+		double score
+		(const array_like &X, const std::vector<double> &y);
 	};
 }
